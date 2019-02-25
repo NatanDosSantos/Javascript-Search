bb9c12479695082481162ff15f9b2ecbe8a2122e
NatanDosSantos <46982207+NatanDosSantos@users.noreply.github.com>
2019-02-25 12:48:12 -0300

    Code

---
+++ Bot/bot.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+function find(txt){
+	window.location.href = "https://www.google.com/search?source=hp&ei=A_NzXKPZMcaj5OUP1_aigAM&q="+txt.value+"&btnK=Pesquisa+Google&oq="+txt.value+"&gs_l=psy-ab.3..0i67l6j0l4.3399.3478..4047...2.0..0.128.352.1j2......0....1..gws-wiz.....6..35i39j0i131.XOri3xtqWQc";
+}
+
+
+
